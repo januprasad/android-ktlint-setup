7bd1e4219dcf0cb25e97a6169f3353976303c9d7
J . K <januprasad@perfectzdigital.com>
2022-08-25 13:00:09 +0530

    Add list

--- app/src/main/java/com/pzd/ktlint_android_sample/MainActivity.kt
+++ app/src/main/java/com/pzd/ktlint_android_sample/MainActivity.kt
@@ -3,38 +3,54 @@ package com.pzd.ktlint_android_sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pzd.ktlint_android_sample.ui.theme.KtlintandroidsampleTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      KtlintandroidsampleTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KtlintandroidsampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    LazyListView()
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun LazyListView() {
+    val messages = remember { mutableListOf("Hello", "Hi", "How are things?") }
+    // val messages = listOf("A", "B", "C")
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageRow(message)
+            }
+        }
+    }
+}
+
+@Composable fun MessageRow(message: String) {
+    Text(text = message)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-  KtlintandroidsampleTheme {
-    Greeting("Android")
-  }
+    KtlintandroidsampleTheme {
+        LazyListView()
+    }
 }
